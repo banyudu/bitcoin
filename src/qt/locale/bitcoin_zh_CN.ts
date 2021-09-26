800eb9b72b11412576028229908fe4a55b9ebea3
Yudu <banyudu@gmail.com>
2021-09-27 07:38:29 +0800

    fix: zh_CN translation

--- src/qt/locale/bitcoin_zh_CN.ts
+++ src/qt/locale/bitcoin_zh_CN.ts
@@ -850,7 +850,7 @@ Signing is only possible with addresses of the type 'legacy'.</source>
     </message>
     <message>
         <source>Encrypt the wallet. The wallet will be encrypted with a passphrase of your choice.</source>
-        <translation>加密钱包。将会使用您指定的密码将钱包钱包。</translation>
+        <translation>加密钱包。将会使用您指定的密码将钱包加密。</translation>
     </message>
     <message>
         <source>Encrypt Wallet</source>
